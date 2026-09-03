54e985b82f4c209828d3fee41536f2fac4b0f316
Ethan Govender <ethangovender31342018@outlook.com>
2026-09-04 00:35:05 +0200

    Initial commit

---
+++ its_chewsday_innit.py
@@ -0,0 +1,11 @@
+print("It's Chew's Day, Innit?")
+
+for i in range(5):
+    print(f"Chew's Day Celebration {i + 1}!")
+
+
+print("Let's have a great time celebrating Chew's Day!")
+
+print("Don't forget to share your Chew's Day moments with friends and family!")
+
+print("WAT DA HELLY?")
